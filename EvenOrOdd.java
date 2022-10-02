1e9f8cbe56f4f1cdd9924a9b5ec2bcc9f34583be
swatikadam-123 <swatikadam907@gmail.com>
2022-10-02 19:19:20 +0530

    added: number is even or odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+
+	public static void main(String[] args) {
+		
+		int num;
+		System.out.println("enter number");
+		Scanner n1=new Scanner(System.in);
+		num=n1.nextInt();
+		
+		
+		if( num%2==0)
+		{
+	System.out.println("num is even");
+		}
+		else
+		
+			System.out.println("num is odd");
+		
+	
+      
+	}
+
+	}
+
+
